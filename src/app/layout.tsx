ef131cb93c3b15245b4dfa28b308bf6bfba406c0
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2024-08-09 19:36:36 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Unmesh",
-  <link rel="icon" href="./images/favicon.jpg">,
+  <link rel="icon" href="./images/favicon.jpg">
   description: "Personal Portfolio",
 };
 
