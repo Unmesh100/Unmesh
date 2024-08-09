1a052515f502a1e9b2f65f69fb51782778c59aa0
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2024-08-09 13:36:50 +0530

    Update words.tsx

--- src/components/words.tsx
+++ src/components/words.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import { FlipWords } from "./ui/words";
 
 export function FlipWordsDemo() {
-  const words = [ "CP'er", 
-    "MERN\u00A0Developer",
-    "WEB-3\u00A0Enthusiast",
-    "DEVOPS\u00A0Engineer",
-    "CODER",
-  "Freelancer"]
+  const words = [ "CP", 
+    "MERN",
+    "WEB-3",
+    "DEVOPS",
+  "EXPLORER"]
 
   return (
     <div className="justify-center items-center px-4">
