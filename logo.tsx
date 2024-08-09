f5c8b42d9a2dd27c44c2cc6271a5a2410c1c1610
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2024-08-09 20:49:03 +0530

    Create logo.tsx

---
+++ logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './assets/logo.png'; 
+const Header: React.FC = () => {
+    return (
+        <header>
+            <img src={logo} alt="Website Logo" style={{ width: '150px', height: 'auto' }} />
+           
+        </header>
+    );
+};
+
+export default Header;
