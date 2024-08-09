4f725185741b42af4d1efe6275fceeda17cf2157
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2024-08-09 13:39:38 +0530

    Update words.tsx

--- src/components/words.tsx
+++ src/components/words.tsx
@@ -6,7 +6,7 @@ export function FlipWordsDemo() {
     "FULLSTACK",
     "WEB-3",
     "DEVOPS"]
-
+ 
   return (
     <div className="justify-center items-center px-4">
       <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400 mt-5 mb-3">
