637eb9823449bc57b6f76b1aaa363fa3db8ea28a
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2024-08-12 01:44:01 +0530

    Update linkpreview.tsx

--- src/components/linkpreview.tsx
+++ src/components/linkpreview.tsx
@@ -7,14 +7,11 @@ export function LinkPreviewDemo() {
   return (
     <div className="flex justify-center items-center h-[40rem] flex-col px-4">
       <p className="text-neutral-500 dark:text-neutral-400 mt-0 text-xl md:text-3xl max-w-3xl mx-auto mb-10">
-        I also create{" "}
+        I also create
         <LinkPreview url="https://github.com/Unmesh100/Unmesh100" className="font-bold">
-          Animated ReadME
+          Animated ReadME On GITHUB.
         </LinkPreview>{" "}
-        and{" "} have worked on projects like{ " "}
-        <LinkPreview url="https://unmesh.vercel.app/projects" className="font-bold">
-         Personal Portfolio.
-        </LinkPreview>{" "}
+       
      
       </p>
    
