97e705144bfd743a8741669627fd25935e9fa578
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2024-08-12 01:53:17 +0530

    Update linkpreview.tsx

--- src/components/linkpreview.tsx
+++ src/components/linkpreview.tsx
@@ -7,9 +7,9 @@ export function LinkPreviewDemo() {
   return (
     <div className="flex justify-center items-center h-[40rem] flex-col px-4">
       <p className="text-neutral-500 dark:text-neutral-400 mt-0 text-xl md:text-3xl max-w-3xl mx-auto mb-10">
-        I also createed 
+        I also createed an 
         <LinkPreview url="https://github.com/Unmesh100/Unmesh100" className="font-bold">
-          <br/> an Animated ReadME On GITHUB.
+          <br/> Animated ReadME On GITHUB.
         </LinkPreview>
        
      
