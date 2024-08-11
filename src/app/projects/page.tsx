cd2b7e63008a4a085f33fd6eeb89065500309295
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2024-08-12 00:34:50 +0530

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -46,7 +46,7 @@ export default function Component() {
                     translateZ={20}
                     translateX={-40}
                     as="a"
-                    href="https://github.com/Unmesh100/UG_JARVIS"
+                    href="ugjarvis.netlify.app/"
                     className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                   >
                     Live Link →
