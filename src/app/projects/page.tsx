756cc5d14a14b71c35c92f9eca4920486f28a7b1
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2024-08-12 01:16:40 +0530

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -77,7 +77,7 @@ export default function Component() {
                   translateZ="60"
                   className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
                 >
-                  Next.js , Drizzle , Tailwind , Shadcn
+                 HTML , Tailwind-CSS , Javascript
                 </CardItem>
                 <CardItem
                   translateZ="100"
@@ -128,7 +128,7 @@ export default function Component() {
                   translateZ="60"
                   className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
                 >
-                  ReactJs , Tailwind 
+                  Typescript , ReactJs , Nextjs , Tailwind , Vercel API
                 </CardItem>
                 <CardItem
                   translateZ="100"
@@ -172,14 +172,14 @@ export default function Component() {
                   translateZ="50"
                   className="text-xl font-bold text-neutral-600 dark:text-white"
                 >
-                  Imaginify -with kairvee
+                  PAPER-PROPOSAL - A Website for Impressive Proposal
                 </CardItem>
                 <CardItem
                   as="p"
                   translateZ="60"
                   className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
                 >
-                  ReactJs , OpenAI API
+                  HTML , CSS , Vanilla Javascript
                 </CardItem>
                 <CardItem
                   translateZ="100"
@@ -188,7 +188,7 @@ export default function Component() {
                   className="w-full mt-4"
                 >
                   <Image
-                    src="/assests/imaginify.png"
+                    src="/assests/PAPER-PROPOSAL.png"
                     height="1000"
                     width="1000"
                     className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
@@ -200,7 +200,7 @@ export default function Component() {
                     translateZ={20}
                     translateX={-40}
                     as="a"
-                    href="https://imaginify-with-kairveee.vercel.app/"
+                    href="https://paper-proposal.vercel.app/"
                     className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                   >
                     Live Link →
@@ -209,7 +209,7 @@ export default function Component() {
                     translateZ={20}
                     translateX={40}
                     as="a"
-                    href="https://github.com/kairveeehh/imaginify-with-kairvee/"
+                    href="https://github.com/Unmesh100/PAPER-PROPOSAL"
                     className="flex items-center px-4 py-2 text-white text-xl font-bold"
                   >
                     <GitHubLogoIcon className="w-8 h-8" />
@@ -217,49 +217,7 @@ export default function Component() {
                 </div>
               </CardBody>
             </CardContainer>
-            <CardContainer className="inter-var">
-              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-transparent dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-                <CardItem
-                  translateZ="50"
-                  className="text-xl font-bold text-neutral-600 dark:text-white"
-                >
-                  Youtube live simulator
-                </CardItem>
-                <CardItem
-                  as="p"
-                  translateZ="60"
-                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                >
-                  NodeJs , FFmpeg , Docker 
-                </CardItem>
-                <CardItem
-                  translateZ="100"
-                  rotateX={0}
-                  rotateZ={-3}
-                  className="w-full mt-4"
-                >
-                  <Image
-                    src="/assests/rtmp simulator.png"
-                    height="1000"
-                    width="1000"
-                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                    alt="thumbnail"
-                  />
-                </CardItem>
-                <div className="flex justify-between items-center mt-20">
-                  
-                  <CardItem
-                    translateZ={20}
-                    translateX={40}
-                    as="a"
-                    href="https://github.com/kairveeehh/legendary-enigma"
-                    className="flex items-center px-4 py-2 text-white text-xl font-bold"
-                  >
-                    <GitHubLogoIcon className="w-8 h-8" />
-                  </CardItem>
-                </div>
-              </CardBody>
-            </CardContainer>
+            
           </div>
           <LinkPreviewDemo />
         </div>
