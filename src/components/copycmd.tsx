b455c5577e69996fcb88b9a08f73bb331843f183
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2024-08-25 10:08:30 +0530

    Update copycmd.tsx

--- src/components/copycmd.tsx
+++ src/components/copycmd.tsx
@@ -8,7 +8,7 @@ import { toast } from "./ui/use-toast";
 const CopyCmd = () => {
     const [copied, setCopied] = useState(false);
     const handleCopyToClipboard = () => {
-        const textToCopy = "npx u.ghosh";
+        const textToCopy = "npx ug28";
         navigator.clipboard
             .writeText(textToCopy)
             .then(() => {
@@ -34,7 +34,7 @@ const CopyCmd = () => {
     return (
         <div onClick={handleCopyToClipboard} className="homepagecontactbtn">
             {copied ? <ClipboardCheck /> : <Clipboard />}
-            npx u.ghosh
+            npx ug28
         </div>
     );
 };
