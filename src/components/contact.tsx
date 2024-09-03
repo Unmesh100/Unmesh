b53770016ff345c0d20cc1699b23e7e902e78752
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2024-09-03 08:08:40 +0530

    Update contact.tsx

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -96,7 +96,7 @@ const ContactMe: React.FC = () => {
         </motion.a>
 
         <motion.a
-          href="https://x.com/Unmesh_Ghosh"
+          href="https://x.com/Unmesh100"
           className="flex flex-col items-center p-4 bg-transparent dark:bg-transparent dark:text-white rounded-lg border border-black dark:border-white shadow-sm hover:bg-gray-200 dark:hover:bg-gray-600 transition transform hover:scale-105 hover:rotate-3"
 
           whileHover={{ scale: 1.1, rotate: -5 }}
